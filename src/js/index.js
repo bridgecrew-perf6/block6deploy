07009041038945e8adfbe672f862eced289aa6f2
Medeya <dea.2412@gmail.com>
2022-01-13 01:18:01 +0300

    added files to master

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '../scss/style.scss';
+
+import './modalcall';
+import './modalfeedback';
+import './readmorebuttons';
+import './sidemenu';
+import './swiper';
+
---
+++ src/js/sidemenu.js
@@ -0,0 +1,24 @@
+ let open = document.querySelector(".header__button");
+ let close = document.querySelector(".menu-header__button");
+
+ let menuContainer = document.querySelector(".menu");
+
+ let menu = document.querySelector(".menu-container");
+
+open.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menuContainer.classList.toggle("menu--hidden");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menuContainer.classList.toggle("menu--hidden");
+});
+
+menuContainer.addEventListener("click", function (evt) {
+   let isClickInside = menu.contains(evt.target);
+
+  if (!isClickInside) {
+    menuContainer.classList.toggle("menu--hidden");
+  }
+});
