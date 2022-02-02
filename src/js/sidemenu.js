fd727612ed4b03827a96107c48b4cfef9ddc266b
Medeya <dea.2412@gmail.com>
2022-02-02 19:48:40 +0300

    files refreshed on master

--- src/js/sidemenu.js
+++ src/js/sidemenu.js
@@ -2,19 +2,20 @@
  let close = document.querySelector(".menu__button");
  let overlay = document.querySelector(".overlay");
  let menuContainer = document.querySelector(".menu");
+ let menuContent = document.querySelector(".menu__container");
 
 
 open.addEventListener("click", function (evt) {
   evt.preventDefault();
   menuContainer.classList.toggle("menu--hidden");
-  
+  menuContent.classList.add("menu__container--zoom-in")
   overlay.classList.toggle("overlay--hidden");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
+  menuContent.classList.remove("menu__container--zoom-in")
   menuContainer.classList.toggle("menu--hidden");
-  
   overlay.classList.toggle("overlay--hidden");
 });
 
@@ -23,7 +24,7 @@ overlay.addEventListener("click", function (evt) {
 
   if (!isClickInside) {
     menuContainer.classList.toggle("menu--hidden");
-    
+    menuContent.classList.remove("menu__container--zoom-in")
     overlay.classList.toggle("overlay--hidden");
   }
 });
